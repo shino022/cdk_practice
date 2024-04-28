863b6f1bde939927c7c50a548b7898acdd3efa76
shin <sshoony95@gmail.com>
2024-04-27 21:04:26 -0400

    Add unit tests

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,10 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
+  },
+  moduleDirectories: [
+    '<rootDir>/node_modules', 
+    '<rootDir>/src/api/authorizer/node_modules',
+    '<rootDir>/src/api/users/node_modules',
+  ],
 };
